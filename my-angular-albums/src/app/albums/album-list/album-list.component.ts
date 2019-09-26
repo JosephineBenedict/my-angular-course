ff53d80304b7dbfa0be29bc247b6dfa481585f5d
Josephine <josephinebenedict@gmail.com>
2019-09-26 13:08:02 -0400

    pipes and observables

--- my-angular-albums/src/app/albums/album-list/album-list.component.ts
+++ my-angular-albums/src/app/albums/album-list/album-list.component.ts
@@ -1,27 +1,39 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Album } from '../album.model';
-import { ALBUMS } from "../albums.data";
+import { ALBUMS } from '../albums.data';
+import { AlbumService } from '../shared/album.service';
 
 @Component({
-  selector: 'app-album-list',
-  templateUrl: './album-list.component.html',
-  styleUrls: ['./album-list.component.css']
+    selector: 'app-album-list',
+    templateUrl: './album-list.component.html',
+    styleUrls: ['./album-list.component.css'],
 })
 export class AlbumListComponent implements OnInit {
-albumsArray: Album [];
-  titleCounter = 1;
-  constructor() { }
-  clickedAlbum: string ;
-  @Output()
-  lastClicked: EventEmitter<string> = new EventEmitter<string>();
+    albumsArray: Album[];
+    titleCounter = 1;
+    clickedAlbum: string;
+    constructor(private albumService: AlbumService) {}
+    @Output()
+    lastClicked: EventEmitter<string> = new EventEmitter<string>();
 
-  ngOnInit(): void {
-    this.albumsArray =  ALBUMS;
-   }
-  parentFunctionHandler(album) {
-    alert('Album ' + album.albumName + ' was sent from the album card component');
-    this.clickedAlbum = album.albumName + ' ' + album.id ;
-    this.lastClicked.emit(this.clickedAlbum);
-}
+    getAlbums() {
+        this.albumService.getAlbums()
+        .subscribe(
+            albums => this.albumsArray = albums,
+            error => console.log("Error: ", error));
+    }
 
+    ngOnInit() {
+        this.getAlbums();
+    }
+
+    parentFunctionHandler(album) {
+        alert(
+            'Album ' +
+                album.albumName +
+                ' was sent from the album card component'
+        );
+        this.clickedAlbum = album.albumName + ' ' + album.id;
+        this.lastClicked.emit(this.clickedAlbum);
+    }
 }
