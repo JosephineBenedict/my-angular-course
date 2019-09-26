8e4e6d3497d3e9376bece812f29dbbc13cae64c0
Josephine <josephinebenedict@gmail.com>
2019-09-26 15:29:58 -0400

    routing

--- my-angular-albums/src/app/albums/album-list/album-list.component.ts
+++ my-angular-albums/src/app/albums/album-list/album-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Album } from '../album.model';
 import { ALBUMS } from '../albums.data';
 import { AlbumService } from '../shared/album.service';
+import { Observable } from 'rxjs';
 
 @Component({
     selector: 'app-album-list',
@@ -9,7 +10,7 @@ import { AlbumService } from '../shared/album.service';
     styleUrls: ['./album-list.component.css'],
 })
 export class AlbumListComponent implements OnInit {
-    albumsArray: Album[];
+    albumsArray: Observable<Album[]>;
     titleCounter = 1;
     clickedAlbum: string;
     constructor(private albumService: AlbumService) {}
@@ -17,10 +18,8 @@ export class AlbumListComponent implements OnInit {
     lastClicked: EventEmitter<string> = new EventEmitter<string>();
 
     getAlbums() {
-        this.albumService.getAlbums()
-        .subscribe(
-            albums => this.albumsArray = albums,
-            error => console.log("Error: ", error));
+        this.albumsArray = this.albumService.getAlbums();
+
     }
 
     ngOnInit() {
